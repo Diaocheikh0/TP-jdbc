aa849b2eef4b2ad82a6c879bb268e2aaad5aa60f
Cheikh D DIao <160619004+Diaocheikh0@users.noreply.github.com>
2025-01-08 23:53:41 +0000

    Update Constantes.java

--- src/config/Constantes.java
+++ src/config/Constantes.java
@@ -3,7 +3,7 @@ package config;
 public class Constantes {
     public static String HOST = "localhost";
     public static String USER = "postgres";
-    public static String PASSWORD = "Diaoprince30";
+    public static String PASSWORD = "DB_PASSWORD";
     public static String DATABASE = "jdbc";
     public static int PORT = 5433;
     public static String URL = "jdbc:postgresql://"+HOST+":"+PORT+"/"+DATABASE;
